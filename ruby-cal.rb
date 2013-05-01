fa7db8b967a72111a6e5decf5441babbac67aafb
melaniehall <melaniehalldesign@gmail.com>
2013-05-01 17:20:08 -0500

    Removed a plethora of instance variables and added arguments to my methods

--- ruby-cal.rb
+++ ruby-cal.rb
@@ -1,76 +1,87 @@
-# month = ARGV[0]
-# year = ARGV[1]
-
-
 class Cal
   attr_accessor :month, :year 
 
-  @@month_array = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-  @@month_number_array = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
-  @@days_array = %w[Su Mo Tu We Th Fr Sa]
-  @@months_with_30_days = %w[September April June Novemeber]
-  @@months_with_31_days = %w[January March May July August September December]
+  MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  MONTH_NUMBER = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  MONTH_NUMBER_STRING = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+  DAYS_ARRAY = %w[Su Mo Tu We Th Fr Sa]
+  MONTHS_WITH_30_DAYS = %w[September April June Novemeber]
+  MONTHS_WITH_31_DAYS = %w[January March May July August September December]
+  YEAR_RANGE = (1800..3000)
 
-  def initialize( month, year )
-    if month.class == Fixnum
-      raise ArgumentError, "#{month} is not a month number (1..12)" if month > 12
-      raise ArgumentError, "#{month} is not a month number (1..12)" if month < 1
-      @month = month
-    elsif month.class == String
-      if @@month_array.include?(month)
-        index = @@month_array.index(month)
-        @month = @@month_array[index]
-      elsif @@month_number_array.include?(month)
-        index = @@month_number_array.index(month)
-        @month = @@month_array[index]
-      else
-      raise ArgumentError, "#{@month} is neither a month number (1..12) nor a name"
+  def initialize( month = nil, year = nil)
+    if month && year
+      month_int = month.to_i
+      year_int = year.to_i
+        if month_int == 0
+          if MONTHS.include?(month)
+            @month = MONTHS.index(month) + 1
+            # index = MONTHS.index(month)
+            # @month = MONTHS[index]
+          else
+            raise ArgumentError, "#{month} is neither a month number (1..12) nor a name"
+          end
+          
+        else 
+          raise ArgumentError, "#{month} is neither a month number (1..12) nor a NAME" if !MONTH_NUMBER.include?(month_int)
+          @month = month_int
+        end
+      @year = year_int
+
+    elsif month && year.nil?
+      month_int = month.to_i
+      @month = "all"
+      @year = month.to_i
+
+      if !YEAR_RANGE.include?(month_int) 
+        raise ArgumentError, "#{month} is an invalid calendar entry."
       end
-    end
-    @year = year
-  end
 
-  def month_name
-    month = @month
-    month_int = month.to_i
-    if month_int > 0
-      return @@month_array[month_int - 1]
     else 
-      if @@month_array.include?(month)
-        index = @@month_array.index(month)
-        return @@month_array[index]
+      @month = 5 #current month
+      @year = 2013 #current year
+    end
+  end
+
+  def month_name month
+    if month > 0
+      return MONTHS[month - 1]
+    else 
+      if MONTHS.include?(month)
+        index = MONTHS.index(month)
+        return MONTHS[index]
       else 
-        raise ArgumentError, "#{@month} is neither a month number (1..12) nor a name"
+        raise ArgumentError, "#{month} is neither a month number (1..12) nor a name"
       end
     end
   end
 
-  def month_to_Fixnum
-    month = @month
-    if month.class == Fixnum
-      return month
-    end
-    if month.class == String
-      if @@month_array.include?(month)
-        index = @@month_array.index(month)
-        return @@month_array.index(month) + 1
-      elsif @@month_number_array.include?(month)
-        return @@month_number_array.index(month) + 1
-      end
-    else
-        raise ArgumentError, "#{@month} is neither a month number (1..12) nor a name"
-    end
-  end
+  # def month_to_Fixnum
+  #   month = @month
+  #   if month.class == Fixnum
+  #     return month
+  #   end
+  #   if month.class == String
+  #     if MONTHS.include?(month)
+  #       index = MONTHS.index(month)
+  #       return MONTHS.index(month) + 1
+  #     elsif MONTH_NUMBER.include?(month)
+  #       return MONTH_NUMBER.index(month) + 1
+  #     end
+  #   else
+  #       raise ArgumentError, "#{@month} is neither a month number (1..12) nor a name"
+  #   end
+  # end
 
-  def days_in_month
-    current_month = month_name
-    current_year = @year
-    if @@months_with_30_days.include?(current_month)
+  def days_in_month (month, year)
+    current_month = month_name(month)
+    current_year = year
+    if MONTHS_WITH_30_DAYS.include?(current_month)
       return 30;
-    elsif @@months_with_31_days.include?(current_month)
+    elsif MONTHS_WITH_31_DAYS.include?(current_month)
       return 31
     elsif current_month === "February"
-      if leap_year? 
+      if self.leap_year?
         return 29
       else 
         return 28
@@ -78,58 +89,58 @@ class Cal
     end
   end
 
-  def print_year_header
-    current_year = @year
-    current_year.center(64)
+  def print_year_header year
+    year = year.to_s
+    year.center(64)
   end
 
-  def print_header
-    month = month_name
-    header = "#{month} #{@year}"
+  def print_header month, year
+    month = month_name(month)
+    header = "#{month} #{year}"
     header.center(20)
   end
 
   def print_weekdays
-    @@days_array.join(" ")
+    DAYS_ARRAY.join(" ")
   end
 
-  def print_3_month_header
+  def print_3_month_header(month)
+    month_start = month - 1
     three_month_header = ""
-    last_index = 2
-    @@month_array[0..last_index].each {|month| month === @@month_array[last_index] ? three_month_header += month.center(20) : three_month_header += month.center(20) + "  "}
+    last_index = month_start + 2
+    MONTHS[month_start..last_index].each {|month| month === MONTHS[last_index] ? three_month_header += month.center(20) : three_month_header += month.center(20) + "  "}
     three_month_header
   end
 
   def print_3_month_week_header
     week_header = ""
     3.times do |x| 
-      x === 3 ? week_header += @@days_array.join(" ") : week_header += @@days_array.join(" ") + "  "
+      x === 3 ? week_header += DAYS_ARRAY.join(" ") : week_header += DAYS_ARRAY.join(" ") + "  "
     end
     return week_header.chomp("  ")
   end
 
-  def print_full_header
-    month_header = print_header
+  def print_full_header month, year
+    month_header = print_header(month, year)
     weekdays = print_weekdays
     full_header = "#{month_header}\n#{weekdays}"
   end
 
   def leap_year?
-    year = @year.to_i
-    if year%400 === 0
+    if @year%400 === 0
        true
-    elsif year%100 === 0 
+    elsif @year%100 === 0 
        false
-    elsif year%4 === 0
+    elsif @year%4 === 0
        true
     else
        false
     end
   end
 
-  def find_start_day
-    m = month_to_Fixnum
-    y = @year.to_i
+  def find_start_day (month, year)
+    m = month
+    y = year
     q = 1 #first day of month
 
     if m < 3
@@ -139,17 +150,61 @@ class Cal
     
     h = ((q + (((m + 1) *26 )/10) + y + (y/4) + (6 * (y/100)) + (y/400))%7)
 
-    start_day = @@days_array[h - 1]
+    start_day = DAYS_ARRAY[h - 1]
   end
 
-  def start_day_index
-    start = find_start_day
-    index_of_start = @@days_array.index(start)
+  def start_day_index(month, year)
+    start = find_start_day(month, year)
+    index_of_start = DAYS_ARRAY.index(start)
   end
 
-  def format_week(week) #weeks are zero indexed
-    index_of_start = start_day_index
-    month_days = days_in_month
+  def format_week(week, month, year) #weeks are zero indexed
+    index_of_start = start_day_index(month, year)
+    month_days = days_in_month(month, year)
+    first_week_days = 7 - index_of_start
+    formatted_week = ""
+    if week == 0 #if first_week? (week == 0)
+      empty_days = 7 - first_week_days
+      empty_days.times { formatted_week += "   " }
+      first_date = 1
+      last_date = first_week_days
+    else
+      first_date = first_week_days + 1 + ((week - 1) * 7)
+      last_date = first_date + 6 > month_days ? month_days : first_date + 6
+
+    end
+    (first_date..last_date).each do | date |
+      formatted_week += " " if date < 10
+      formatted_week += "#{date}"
+      formatted_week += " " unless date == last_date
+    end
+    if formatted_week.length < 20
+      extra_space = 20 - formatted_week.length
+      extra_space.times {formatted_week += " "}
+    end  
+    formatted_week
+
+  end
+
+  def format_weeks_for_3_months(week, year) #weeks are zero indexed
+    month_start = week + 2
+    month_end = month_start + 2
+    formatted_week = ""
+    while week < 6 
+
+    MONTHS[0..2].each{|x| formatted_week += format_week(week, MONTHS.index(x) + 1, year) + "  "} 
+    formatted_week = formatted_week.chomp("  ")
+    formatted_week = "#{formatted_week}\n"
+    week += 1
+    end
+    
+    return formatted_week.chomp("  ")
+
+  end
+
+  def format_week_for_year(week, month, year) #weeks are zero indexed
+    index_of_start = start_day_index(month, year)
+    month_days = days_in_month(month, year)
     first_week_days = 7 - index_of_start
     formatted_week = ""
     if week == 0 #if first_week? (week == 0)
@@ -167,46 +222,11 @@ class Cal
       formatted_week += " " unless date == last_date
     end
     formatted_week
-
   end
 
-  def format_week_1_for_3_months(week) #weeks are zero indexed
-    if week == 0
-      formatted_week = ""
-    @@month_array[0..2].each{|x| formatted_week += format_week_for_year(0, x) + "  "} 
-    return formatted_week
-    else
-      return "not week one"
-    end
-
-  end
-
-  def format_week_for_year(week, month) #weeks are zero indexed
-    @month = month
-    index_of_start = start_day_index
-    month_days = days_in_month
-    first_week_days = 7 - index_of_start
-    formatted_week = ""
-    if week == 0 #if first_week? (week == 0)
-      empty_days = 7 - first_week_days
-      empty_days.times { formatted_week += "   " }
-      first_date = 1
-      last_date = first_week_days
-    else
-      first_date = first_week_days + 1 + ((week - 1) * 7)
-      last_date = first_date + 6 > month_days ? month_days : first_date + 6
-    end
-    (first_date..last_date).each do | date |
-      formatted_week += " " if date < 10
-      formatted_week += "#{date}"
-      formatted_week += " " unless date == last_date
-    end
-    formatted_week
-  end
-
-  def format_month
+  def format_month(month, year)
     formatted_month = ""
-    (0..5).each{|x| formatted_month += format_week(x) + "\n"}
+    (0..5).each{|x| formatted_month += format_week(x, month, year) + "\n"}
     formatted_month 
   end
 
@@ -215,16 +235,23 @@ class Cal
 
   end
 
-  def print_cal
-    puts print_full_header
-    puts format_month
+  def format_cal
+    puts print_year_header(year)
+    puts print_3_month_header(1)
+    puts print_3_month_week_header
+    puts format_weeks_for_3_months(0, year)
+    puts print_3_month_header(4)
+    puts print_3_month_week_header
+    puts format_weeks_for_3_months(0, year)
+    # puts print_full_header(month, year)
+    # puts format_month(month, year)
   end
 
 if __FILE__ == $0
   month = ARGV[0]
   year = ARGV[1]
   cal = Cal.new(month, year)
-  cal.print_cal
+  cal.format_cal
 end
 
 end
--- test/test_units.rb
+++ test/test_units.rb
@@ -3,9 +3,19 @@ require './ruby-cal.rb'
 
 class CalTest < Test::Unit::TestCase
 
-  def test_01_initialize_stores_month
+  def test_01a_initialize_stores_month
     calendar = Cal.new(5, 2013)
     assert_equal(5, calendar.month)
+  end
+
+  def test_01b_initialize_stores_month
+    calendar = Cal.new("January", "2013")
+    assert_equal(1, calendar.month)
+  end
+
+  def test_01c_initialize_stores_month
+    calendar = Cal.new("1", "2013")
+    assert_equal(1, calendar.month)
   end
 
   def test_02_initialize_stores_year
@@ -13,46 +23,47 @@ class CalTest < Test::Unit::TestCase
     assert_equal(2013, calendar.year)
   end
 
-  def test_03a_invalid_argument_throws_error
+  def test_03a_invalid_month_number_throws_error
     assert_raise ArgumentError do
     calendar = Cal.new(0, 2021)
     end
   end 
 
-  def test_03b_invalid_argument_throws_error
-    assert_raise ArgumentError do
-    calendar = Cal.new("Janruary", 2021)
-    end
+  def test_03b_initialize_stores_year_only
+    calendar = Cal.new(2021)
+    assert_equal(2021, calendar.year)
+    assert_equal("all", calendar.month)
   end 
 
-  def test_04a_month_name # can get rid of this-- inputs from command line are always strings.
-    calendar = Cal.new(5, 2013)
-    month = calendar.month_name
-    assert_equal("May", month)
+  def test_04a_stores_default_month
+    calendar = Cal.new()
+    assert_equal(5, calendar.month)
   end 
+
+  def test_04b_stores_default_month
+    calendar = Cal.new()
+    assert_equal(2013, calendar.year)
+  end 
+
 
   def test_04b_month_name
     calendar = Cal.new("January", 2013)
-    month = calendar.month_name
-    assert_equal("January", month)
+    assert_equal("January", calendar.month_name(1))
+  end 
+
+  def test_04c_month_name
+    calendar = Cal.new("1", "2013")
+    assert_equal("February", calendar.month_name(2))
   end 
 
   def test_05a_print_header
     calendar = Cal.new(1, 2013)
-    header = calendar.print_header
-    assert_equal("    January 2013    ", header)
+    assert_equal("    January 2013    ", calendar.print_header(1, 2013))
   end
 
   def test_05b_print_header
     calendar = Cal.new(2, 2013)
-    header = calendar.print_header
-    assert_equal("   February 2013    ", header)
-  end
-
-  def test_05c_print_header
-    calendar = Cal.new(3, 2013)
-    header = calendar.print_header
-    assert_equal("     March 2013     ", header)
+    assert_equal("   February 2013    ", calendar.print_header(2, 2013))
   end
 
   def test_06_print_weekdays
@@ -62,7 +73,7 @@ class CalTest < Test::Unit::TestCase
 
   def test_07_print_full_header
     calendar = Cal.new(3, 2013)
-    assert_equal("     March 2013     \nSu Mo Tu We Th Fr Sa", calendar.print_full_header)
+    assert_equal("     March 2013     \nSu Mo Tu We Th Fr Sa", calendar.print_full_header(3, 2013))
   end
 
   def test_08_test_if_leap_year
@@ -78,136 +89,119 @@ class CalTest < Test::Unit::TestCase
 
   def test_09a_find_start_day
     calendar = Cal.new(4, 2013)
-    assert_equal("Mo", calendar.find_start_day)
+    assert_equal("Mo", calendar.find_start_day(4, 2013))
   end
 
-  def test_09b_test_find_start_day
+  def test_09b_find_start_day
     calendar = Cal.new(3, 2000)
-    assert_equal("We", calendar.find_start_day)
+    assert_equal("We", calendar.find_start_day(3, 2000))
   end
 
   def test_09c_test_find_start_day
     calendar = Cal.new(1, 2000)
-    assert_equal("Sa", calendar.find_start_day)
+    assert_equal("Sa", calendar.find_start_day(1, 2000))
   end
 
   def test_10_find_start_day_index
     calendar = Cal.new(1, 2000)
-    assert_equal(6, calendar.start_day_index)
+    assert_equal(6, calendar.start_day_index(1, 2000))
   end 
 
-  def test_11a_month_to_Fixnum
-    calendar = Cal.new(4, 2013)
-    assert_equal(4, calendar.month_to_Fixnum)
-  end
-
-  def test_11b_month_to_Fixnum
-    calendar = Cal.new("May", 2013)
-    assert_equal(5, calendar.month_to_Fixnum)
-  end
-
-  def test_11c_month_to_Fixnum
-    calendar = Cal.new("5", 2013)
-    assert_equal(5, calendar.month_to_Fixnum)
-  end
-
-
-
   def test_15a_days_in_month_for_30_day_month
-    calendar = Cal.new("4", 2013)
-    assert_equal(30, calendar.days_in_month)
+    calendar = Cal.new(4, 2013)
+    assert_equal(30, calendar.days_in_month(4, 2013))
   end
 
   def test_15b_days_in_month_for_31_day_month
-    calendar = Cal.new("5", 2013)
-    assert_equal(31, calendar.days_in_month)
+    calendar = Cal.new(5, 2013)
+    assert_equal(31, calendar.days_in_month(5, 2013))
   end
 
   def test_15c_days_in_month_for_february_leap_year
     calendar = Cal.new("2", 2012)
-    assert_equal(29, calendar.days_in_month)
+    assert_equal(29, calendar.days_in_month(2, 2012))
   end
 
   def test_15d_days_in_month_for_february_common_year
     calendar = Cal.new("2", 2013)
-    assert_equal(28, calendar.days_in_month)
+    assert_equal(28, calendar.days_in_month(2, 2013))
   end
 
 
 # Month with 30 Days
-  def test_12a_format_week0
+  def test_16_format_week0
     calendar = Cal.new(4, 2013)
-    assert_equal("    1  2  3  4  5  6", calendar.format_week(0))
+    assert_equal("    1  2  3  4  5  6", calendar.format_week(0, 4, 2013))
   end
 
   def test_12b_format_week1
     calendar = Cal.new(4, 2013)
-    assert_equal(" 7  8  9 10 11 12 13", calendar.format_week(1))
+    assert_equal(" 7  8  9 10 11 12 13", calendar.format_week(1, 4, 2013))
   end
 
   def test_12c_format_week2
     calendar = Cal.new(4, 2013)
-    assert_equal("14 15 16 17 18 19 20", calendar.format_week(2))
+    assert_equal("14 15 16 17 18 19 20", calendar.format_week(2, 4, 2013))
   end
 
   def test_12d_format_week3
     calendar = Cal.new(4, 2013)
-    assert_equal("21 22 23 24 25 26 27", calendar.format_week(3))
+    assert_equal("21 22 23 24 25 26 27", calendar.format_week(3, 4, 2013))
   end
 
   def test_12e_format_week4
     calendar = Cal.new(4, 2013)
-    assert_equal("28 29 30            ", calendar.format_week(4))
+    assert_equal("28 29 30            ", calendar.format_week(4, 4, 2013))
   end
 
   def test_12f_format_week5
     calendar = Cal.new(4, 2013)
-    assert_equal("                    ", calendar.format_week(5))
+    assert_equal("                    ", calendar.format_week(5, 4, 2013))
   end
 
   def test_13a_format_week0
     calendar = Cal.new(5, 2013)
-    assert_equal("          1  2  3  4", calendar.format_week(0))
+    assert_equal("          1  2  3  4", calendar.format_week(0, 5, 2013))
   end
 
   def test_13b_format_week1
     calendar = Cal.new(5, 2013)
-    assert_equal(" 5  6  7  8  9 10 11", calendar.format_week(1))
+    assert_equal(" 5  6  7  8  9 10 11", calendar.format_week(1, 5, 2013))
   end
 
   def test_13c_format_week2
     calendar = Cal.new(5, 2013)
-    assert_equal("12 13 14 15 16 17 18", calendar.format_week(2))
+    assert_equal("12 13 14 15 16 17 18", calendar.format_week(2, 5, 2013))
   end
 
   def test_13d_format_week3
     calendar = Cal.new(5, 2013)
-    assert_equal("19 20 21 22 23 24 25", calendar.format_week(3))
+    assert_equal("19 20 21 22 23 24 25", calendar.format_week(3, 5, 2013))
   end
 
   def test_13e_format_week4
     calendar = Cal.new(5, 2013)
-    assert_equal("26 27 28 29 30 31    ", calendar.format_week(4))
+    assert_equal("26 27 28 29 30 31   ", calendar.format_week(4, 5, 2013))
   end
 
   def test_13f_format_week5
     calendar = Cal.new(5, 2013)
-    assert_equal("                    ", calendar.format_week(5))
+    assert_equal("                    ", calendar.format_week(5, 5, 2013))
   end
 
   def test_14_format_month
     calendar = Cal.new(5, 2013)
-    assert_equal("          1  2  3  4\n 5  6  7  8  9 10 11\n12 13 14 15 16 17 18\n19 20 21 22 23 24 25\n26 27 28 29 30 31    \n                    ", calendar.format_month)
+    assert_equal("          1  2  3  4\n 5  6  7  8  9 10 11\n12 13 14 15 16 17 18\n19 20 21 22 23 24 25\n26 27 28 29 30 31    \n                    ", calendar.format_month(5, 2013))
   end
 
   def test_15_print_year_header
     calendar = Cal.new("3", "2013")
-    assert_equal("2013".center(64), calendar.print_year_header)
+    assert_equal("2013".center(64), calendar.print_year_header(2013))
   end
 
   def test_16_print_3_month_header
     calendar = Cal.new("3", "2013")
-    assert_equal("January".center(20) + "  " + "February".center(20) + "  " + "March".center(20), calendar.print_3_month_header)
+    assert_equal("January".center(20) + "  " + "February".center(20) + "  " + "March".center(20), calendar.print_3_month_header(1))
   end
 
   def test_17_print_3_month_week_header
@@ -217,12 +211,12 @@ class CalTest < Test::Unit::TestCase
 
   def test_18_format_3_month_week_1
     calendar = Cal.new("3", "2013")
-    assert_equal("       1  2  3  4  5                  1  2                  1  2", calendar.format_week_1_for_3_months(0))
+    assert_equal("       1  2  3  4  5                  1  2                  1  2", calendar.format_weeks_for_3_months(0, 2013))
   end
 
   def test_19_print_3_month_week_2
     calendar = Cal.new("3", "2013")
-    assert_equal(" 6  7  8  9 10 11 12   3  4  5  6  7  8  9   3  4  5  6  7  8  9", calendar.format_week_1_for_3_months(1))
+    assert_equal(" 6  7  8  9 10 11 12   3  4  5  6  7  8  9   3  4  5  6  7  8  9", calendar.format_weeks_for_3_months(1, 2013))
   end
 end
 
